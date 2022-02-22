cef54632f6d2f6708020e1966acad7eb2882045b
Marcel Rädeker <marcel.raedeker@gmail.com>
2022-02-22 09:21:01 +0100

    Started work on filesys

--- auxil/filesys.h
+++ auxil/filesys.h
@@ -2,4 +2,18 @@
 #ifndef H8SDB_FILESYS_HEADER
 #define H8SDB_FILESYS_HEADER
 
+#ifndef COMFY_STRING_HEADER
+#error "Comfystring required but not included.\n Get it from https://github.com/mradek290/comfystring.git"
+#endif
+
+typedef struct {
+    char* AbsolutePath;
+    char* RelativePath;
+    char* Extension;
+    unsigned long long Size;
+    _Bool IsFile;
+} h8sFileInfo;
+
+h8sFileInfo* EnumerateDirectory( const char*, unsigned* );
+
 #endif
